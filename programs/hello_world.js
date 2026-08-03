9e7faabe8b4330ef01bb6f9a43f53263bba155fd
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-03 18:13:34 +0700

    feat: add Hello World (JavaScript)

---
+++ programs/hello_world.js
@@ -0,0 +1,7 @@
+// hello_world.js
+// Program pertama: sapaan sederhana.
+// Jalankan: node hello_world.js
+
+const name = 'World';
+console.log('Hello, ' + name + '!');
+console.log('Selamat belajar JavaScript.');
